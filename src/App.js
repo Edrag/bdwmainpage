5c9842380302adab6acb8a931278d64544939686
ETechDev <Edrag@users.noreply.github.com>
2020-04-06 20:04:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  redirect = () => {
+    window.location.href='/berrycheckin';
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button className="mainMenu" onClick={this.redirect}>Check Berries In</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
